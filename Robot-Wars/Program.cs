7204aa5e9508a5b5f069846c542d23604d262b46
Joaquim Guimaraes <jgilfnuk@gmail.com>
2021-09-25 13:56:02 +0100

    Initialized Robot-Wars project template created from ``dotnet new console``

---
+++ Robot-Wars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Robot_Wars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
