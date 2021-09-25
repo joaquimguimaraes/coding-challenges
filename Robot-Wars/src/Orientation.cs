d2f78e15cef6394ee1da5d1bfc38b84abbdc1355
Joaquim Guimaraes <jgilfnuk@gmail.com>
2021-09-25 18:55:08 +0100

    Initialises Orientation enum Defines North, West, South, East directions

---
+++ Robot-Wars/src/Orientation.cs
@@ -0,0 +1,26 @@
+
+namespace RobotWars
+{
+    /// <summary>Enum <c>Orientation</c> represents the direction a robot is facing 
+    /// in a two-dimensional battle arena </summary>
+    ///
+    public enum Orientation
+    {
+        /// <summary>
+        /// North
+        /// </summary>
+        N,
+        /// <summary>
+        /// West
+        /// </summary>
+        W,
+        /// <summary>
+        /// South
+        /// </summary>
+        S,
+        /// <summary>
+        /// East
+        /// </summary>
+        E
+    }
+}
