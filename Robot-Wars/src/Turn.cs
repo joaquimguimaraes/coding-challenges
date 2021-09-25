914b2e63137e270297913fe5239083080591e58d
Joaquim Guimaraes <jgilfnuk@gmail.com>
2021-09-25 18:58:17 +0100

    Defines Turn enum either L (Left) or R (right)

---
+++ Robot-Wars/src/Turn.cs
@@ -0,0 +1,18 @@
+namespace RobotWars
+{
+    /// <summary>
+    /// Enum <c>Turn</c> describes which way the robot is turning. 
+    /// Either R (right) or L (left)
+    /// </summary>
+    enum Turn
+    {
+        /// <summary>
+        /// Right
+        /// </summary>
+        R = 1,
+        /// <summary>
+        /// Left
+        /// </summary>
+        L = -1
+    }
+}
