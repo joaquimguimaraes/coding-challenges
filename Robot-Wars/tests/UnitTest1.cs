670e56dbb08006bb554c67aadcf39c33de8aa3aa
Gil Nobrega <jgilfnuk@gmail.com>
2021-09-26 18:17:01 +0200

    Early implementation of Unit Test

--- Robot-Wars/tests/UnitTest1.cs
+++ Robot-Wars/tests/UnitTest1.cs
@@ -1,14 +1,35 @@
 using System;
 using Xunit;
 
-namespace tests
+namespace Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public bool Test1()
         {
+            //test input
+            string input = @"
+5 5
+1 2 N
+LMLMLMLMM
+3 3 E
+MMRMMRMRRM
+";
 
+            string expectedOutput = @"
+1 3 N
+5 1 E
+";
+
+            string output = RobotWars.Program.ReadInput(input);
+
+            bool result = output == expectedOutput;
+
+            Assert.False(result, $"Output:\n'{output}'\ndoes not match expected output:\n'{expectedOutput}'");
+            Assert.True(result, $"Output:\n'{output}'\nmatches expected output:\n'{expectedOutput}'");
+             
+            return result;
         }
     }
 }
