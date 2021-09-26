082519873ed794564d32b91dc7a956d122f35477
Gil Nobrega <jgilfnuk@gmail.com>
2021-09-26 18:16:47 +0200

    Splits function that processes input from main function Program is now able to process user input directly

--- Robot-Wars/Program.cs
+++ Robot-Wars/Program.cs
@@ -6,26 +6,39 @@ namespace RobotWars
     /// <summary>
     /// Main program which reads input
     /// </summary>
-    class Program
+    public class Program
     {
-            // Search predicate returns true if a character is space character
-    private static bool isSpaceCharacter(char character)
-    {
-        return character ==' ';
-    }
+        // Search predicate returns true if a character is space character
+        private static bool isSpaceCharacter(char character)
+        {
+            return character == ' ';
+        }
+
         static void Main(string[] args)
+        {
+            //Stops asking for user input after detecting END
+            const string breakWord = "END";
+
+            Console.WriteLine($"Please provide input.\nType '{breakWord}' and press enter when you are finished\n\nInput:");
+            string input = "";
+            string inputLine = "";
+
+            while (!inputLine.Contains(breakWord))
+            {
+                inputLine = Console.ReadLine();
+
+                if (!inputLine.Contains(breakWord))
+                    input += inputLine + Environment.NewLine;
+                else
+                    input += inputLine.Replace(breakWord, "") + Environment.NewLine;
+            }
+
+            Console.WriteLine("\nOutput:\n" + ReadInput(input));
+        }
+        public static string ReadInput(string input)
         {
             //initializes empty list of robots
             List<Robot> robots = new List<Robot>();
-
-            //test input
-            string input = @"
-5 5
-1 2 N
-LMLMLMLMM
-3 3 E
-MMRMMRMRRM
-            ";
 
             string[] inputLines = input.Split(new[] { Environment.NewLine, "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
 
@@ -64,14 +77,14 @@ MMRMMRMRRM
                         for (int k = 0; k < lineArgs.Count; k++)
                         {
                             char currentArg = lineArgs[k];
-                            
+
                             //if character is M then moves the robot forward
                             if (currentArg == 'M')
                                 currentRobot.MoveForward();
                             //if the character is not M then it must be either L or R, so it spins the robot in that direction
                             else
                                 currentRobot.Spin(Enum.Parse<Turn>(currentArg.ToString()));
-                            
+
                         }
 
                         robots.Add(currentRobot);
@@ -79,10 +92,13 @@ MMRMMRMRRM
                 }
             }
 
+            string output = "";
+
             foreach (Robot robot in robots)
-            {
-                Console.WriteLine($"{robot.position.x} {robot.position.y} {robot.orientation}");
-            }
+
+                output += $"{robot.position.x} {robot.position.y} {robot.orientation}\n";
+
+            return output;
 
         }
     }
