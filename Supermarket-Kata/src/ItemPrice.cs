1111c3be41f1ab67dabe8cc2fc6248d0180fc640
Joaquim Guimaraes <jgilfnuk@gmail.com>
2021-09-26 23:38:30 +0100

    Rnenamed ItemInfo to ItemPrice namespace

---
+++ Supermarket-Kata/src/ItemPrice.cs
@@ -0,0 +1,38 @@
+
+
+namespace SupermarketKata
+{
+    /// <summary>
+    /// Struct <c>ItemPrice</c> contains information about unit price and special price of a specific SKU
+    /// </summary>
+    struct ItemPrice
+    {
+        /// <summary>
+        /// SKU of item
+        /// </summary>
+        public readonly string sku { get; init; }
+
+        /// <summary>
+        /// Unit price of item when purchased individually
+        /// </summary>
+        public readonly int unitPrice { get; init; }
+
+        /// <summary>
+        /// OPTIONAL: Special price of item when purchased in a bundle.
+        /// No special price if null
+        /// </summary>
+        public readonly SpecialPrice? specialPrice { get; init; }
+
+        /// <summary>
+        /// Initializes ItemInfo object (read-only)
+        /// SpecialPrice defaults to null -> means there is not a special offer when buying item in a bundle
+        /// </summary>
+        public ItemPrice(string sku, int unitPrice, SpecialPrice? specialPrice = null)
+        {
+            this.sku = sku;
+            this.unitPrice = unitPrice;
+            this.specialPrice = specialPrice;
+        }
+    }
+
+}
